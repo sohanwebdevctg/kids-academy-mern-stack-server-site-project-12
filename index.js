7001eac30085526bcd0c3b14c9f400af03498bb1
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-06 22:55:40 +0600

    create stripe client secrect

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const stripe = require("stripe")(process.env.STRIPE_SECRECT_KEY)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
@@ -246,6 +246,21 @@ async function run() {
       const query = {_id: new ObjectId(id)};
       const result = await selectedClassesCollection.deleteOne(query);
       res.send(result);
+    })
+
+
+    // create-payment-intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const {price} = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"]
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      });
     })
 
     // Send a ping to confirm a successful connection
